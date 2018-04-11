2130a985b97db440e7a03500a90c705704d8d683
lord007pirate <tkhisham007@gmail.com>
2018-04-11 07:01:59 +0530

    renaming index file

---
+++ process.php
@@ -0,0 +1,26 @@
+<?php
+    // Get values from form in login.php file
+    $username =$_POST['user'];
+    $password =$_POST['pass'];
+
+    // to prevent mysql injection
+
+    $username = stripcslashes($username);
+    $password = stripcslashes($password);
+    $username = mysql_real_escape_string($username);
+    $password = mysql_real_escape_string($password);
+
+    // conect to the server and select database
+    mysql_connect("localhost", "root", "");
+    mysql_select_db("login");
+
+    // Query the database for username
+    $result = mysql_query("select * from users where username = '$username' and password = '$password'")
+                      or die("Failed to query database ".mysql_error());
+    $row= mysql_fetch_array($result);
+    if ($row['username'] == $username && $row['password'] == $password ){
+          echo "Login success!! Welcome ".$row['username'];
+    } else {
+      echo "Failed to login!";
+    }
+ ?>
